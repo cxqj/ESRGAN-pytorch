52e14db2ab688e5c5d5dfd04fa782bbacf7372cb
cxqj <43702702+cxqj@users.noreply.github.com>
2021-02-23 16:51:03 +0800

    Update dataloader.py

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -8,3 +8,4 @@ def get_loader(image_size, scale, batch_size, sample_batch_size):
 
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     return train_loader
+
